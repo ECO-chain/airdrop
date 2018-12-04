08873b6a9374e8dc101d8a0588d4d765a43498ba
akis <akis@ecoc.io>
2018-12-04 17:28:24 +0700

    creating transaction

--- test/test-env.js
+++ test/test-env.js
@@ -1,16 +1,12 @@
 var assert = require('assert');
 require('dotenv').config();
 
-it('read env variables', function(){
-    assert.equal(process.env.TESTENV, 'testenv value');
-});
-
-/*
-describe('next test', function() {
-  describe('test1', function() {
-    it('test env', function() {
+describe('env variables', function() {
+    it('read test env', function() {
       assert.equal(true, true);
     });
-  });
+    it('read env prov', function(){
+    assert.equal(process.env.INFURA_ROPSTEN_ENDPOINT, 'https://ropsten.infura.io/v3/ddb4e9625f68463ba2ba99232d3a06c1');
 });
-*/
+
+  });
